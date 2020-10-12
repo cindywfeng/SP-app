0b8c7847d3db711f8b56642ddd03c0f01d48be85
cindywfeng <cindy.feng.18@ucl.ac.uk>
2020-10-12 16:21:52 +0100

    fix bug url path for Items class

--- routineApp/routines/models.py
+++ routineApp/routines/models.py
@@ -6,8 +6,11 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=50)
     brand = models.CharField(max_length=50)
-    description = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
     created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Routine(models.Model):
@@ -28,3 +31,6 @@ class Routine(models.Model):
     itemid7 = models.ForeignKey(
         Item, on_delete=models.CASCADE,  blank=True, related_name='itemid7')
     comment = models.CharField(max_length=300, blank=True)
+
+    def __str__(self):
+        return self.name
--- routineApp/routines/urls.py
+++ routineApp/routines/urls.py
@@ -1,6 +1,8 @@
 from rest_framework import routers
 from .api import RoutineViewSet
+from .api import ItemViewSet
 router = routers.DefaultRouter()
 router.register('api/routines', RoutineViewSet, 'routines')
+router.register('api/items', ItemViewSet, 'items')
 
 urlpatterns = router.urls
