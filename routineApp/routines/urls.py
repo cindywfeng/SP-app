be423930ff0eb39092190e55496a5e78c70f74ae
cindywfeng <cindy.feng.18@ucl.ac.uk>
2020-10-12 14:51:59 +0100

    complete CRUD rest framework API

---
+++ routineApp/routines/urls.py
@@ -0,0 +1,6 @@
+from rest_framework import routers
+from .api import RoutineViewSet
+router = routers.DefaultRouter()
+router.register('api/routines', RoutineViewSet, 'routines')
+
+urlpatters = router.urls
