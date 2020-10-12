55b981d102c92c79e207c6223adb11b1525158f8
cindywfeng <cindy.feng.18@ucl.ac.uk>
2020-10-12 14:43:23 +0100

    create models and makemigrations

--- routineApp/routines/models.py
+++ routineApp/routines/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Routine(models.Model):
+    date = models.DateTimeField()
+    timeofday = models.CharField(max_length=2)
+    itemid1 = models.PositiveIntegerField()
+    itemid2 = models.PositiveIntegerField()
+    itemid3 = models.PositiveIntegerField(blank=True)
+    itemid4 = models.PositiveIntegerField(blank=True)
+    itemid5 = models.PositiveIntegerField(blank=True)
+    itemid6 = models.PositiveIntegerField(blank=True)
+    itemid7 = models.PositiveIntegerField(blank=True)
+    comment = models.CharField(max_length=300, blank=True)
