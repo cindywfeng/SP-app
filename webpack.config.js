ad2b66a31a4758a83f2caaaa1f6fcf6ae969d30c
cindywfeng <cindy.feng.18@ucl.ac.uk>
2020-10-13 11:49:18 +0100

    fix bug webpack run dev

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,9 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: { loader: "babel-loader" },
       },
+      { test: /\.css$/, loader: ["style-loader", "css-loader"] },
     ],
   },
 };
