f1e845d7b42997301d51fcbdccd8140f05520702
cindywfeng <cindy.feng.18@ucl.ac.uk>
2020-10-15 17:17:47 +0100

    validate data for hashing

--- routineApp/accounts/serializers.py
+++ routineApp/accounts/serializers.py
@@ -19,4 +19,7 @@ class RegisterSerializer(serializers.RegisterSerializer):
         fields = ('id', 'username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data['username'], validated_data['email'], validated_data['password'])
 # Login Serializer
