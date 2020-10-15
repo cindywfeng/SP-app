6e566af06056a96970e4ea9c6fe33f90500b1bcd
cindywfeng <cindy.feng.18@ucl.ac.uk>
2020-10-15 12:18:06 +0100

    create options config for react-alert

--- routineApp/frontend/src/components/App.js
+++ routineApp/frontend/src/components/App.js
@@ -8,14 +8,33 @@ import Dashboard from "./routines/Dashboard";
 import { Provider } from "react-redux";
 import store from "../store";
 
+// toastify [ does not work atm]
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// react-alert
+import { transitions, positions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-mui";
+
+// Alert options config
+const options = {
+  position: positions.TOP_RIGHT,
+  timeout: 3000,
+  offset: "30px",
+  transition: transitions.SCALE,
+};
+
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Fragment>
-          <Header />
-          <Dashboard />
-        </Fragment>
+        <AlertProvider template={AlertTemplate}>
+          <Fragment>
+            <ToastContainer />
+            <Header />
+            <Dashboard />
+          </Fragment>
+        </AlertProvider>
       </Provider>
     );
   }
