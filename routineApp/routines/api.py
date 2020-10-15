494edd9fe899936a61d35ee9789e192a5dd244aa
cindywfeng <cindy.feng.18@ucl.ac.uk>
2020-10-15 16:54:15 +0100

    make viewsets have permission constraints

--- routineApp/routines/api.py
+++ routineApp/routines/api.py
@@ -9,16 +9,28 @@ from .serializers import ItemSerializer
 
 
 class RoutineViewSet(viewsets.ModelViewSet):
-    queryset = Routine.objects.all()
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
     serializer_class = RoutineSerializer
 
+    def get_queryset(self):
+        return self.request.user.items.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+# Item ViewSet
+
 
 class ItemViewSet(viewsets.ModelViewSet):
-    queryset = Item.objects.all()
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
     serializer_class = ItemSerializer
+
+    def get_queryset(self):
+        return self.request.user.routines.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
