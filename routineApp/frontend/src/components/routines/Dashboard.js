731e8dfa7b2e2828b88b31ac9b26367a31f992a1
cindywfeng <cindy.feng.18@ucl.ac.uk>
2020-10-19 21:23:29 +0100

    add css routine list

--- routineApp/frontend/src/components/routines/Dashboard.js
+++ routineApp/frontend/src/components/routines/Dashboard.js
@@ -19,7 +19,7 @@ export default function Dashboard() {
         </h1>
       </div>
       {/* <EmailForm /> */}
-      <RoutineForm />
+      {/* <RoutineForm /> */}
       {/* <ItemForm /> */}
       <Routines />
     </Fragment>
--- routineApp/frontend/src/components/routines/Routines.js
+++ routineApp/frontend/src/components/routines/Routines.js
@@ -3,6 +3,26 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { getRoutines, deleteRoutine } from "../../actions/routines";
 
+import "./Routines.css";
+
+// comments
+// {
+//   this.props.routines.map((routine) => (
+//     <div key={routine.id}>
+//       <h1>
+//         {routine.date} | {routine.timeofday} | {routine.itemid1} |
+//         {routine.itemid2}
+//       </h1>
+//       <button
+//         onClick={this.props.deleteRoutine.bind(this, routine.id)}
+//         className="delete-btn"
+//       >
+//         Delete
+//       </button>
+//     </div>
+//   ));
+// }
+
 export class Routines extends Component {
   static propTypes = {
     routines: PropTypes.array.isRequired,
@@ -15,23 +35,60 @@ export class Routines extends Component {
   }
   render() {
     return (
-      <Fragment>
-        <h1>List of all routines</h1>
-        {this.props.routines.map((routine) => (
-          <div key={routine.id}>
-            <h1>
-              {routine.date} | {routine.timeofday} | {routine.itemid1} |
-              {routine.itemid2}
-            </h1>
-            <button
-              onClick={this.props.deleteRoutine.bind(this, routine.id)}
-              className="delete-btn"
-            >
-              Delete
-            </button>
-          </div>
-        ))}
-      </Fragment>
+      <div className="routines-area">
+        <h1 className="title-text1">Recent Skincare Routines</h1>
+        {/* Routines in cards */}
+        {/* skincare items images */}
+        {this.props.routines.map((routine) => {
+          return (
+            <div className="individual-routine" key={routine.id}>
+              <h2 className="title-text2">Date: {routine.date}</h2>
+              <h3 className="title-text3">{routine.timeofday} routine</h3>
+              <h3 className="title-text4">{routine.comment}</h3>
+              <div className="container1">
+                <ul className="progressbar1">
+                  <li>
+                    <img src="https://www.flaticon.com/svg/static/icons/svg/3520/3520578.svg" />
+                  </li>
+                  <li>
+                    <img src="https://www.flaticon.com/svg/static/icons/svg/2323/2323189.svg" />
+                  </li>
+                  <li>
+                    <img src="https://www.flaticon.com/svg/static/icons/svg/3440/3440983.svg" />
+                  </li>
+                  <li>
+                    <img src="https://www.flaticon.com/svg/static/icons/svg/3368/3368350.svg" />
+                  </li>
+                </ul>
+              </div>
+              <div className="container">
+                <ul className="progressbar">
+                  <li>Step 1. Cleanser</li>
+                  <li>Step 2. Serum</li>
+                  <li>Step 3. Treatment</li>
+                  <li>Step 4. Moisturizer</li>
+                </ul>
+              </div>
+              <div className="container2">
+                <ul className="progressbar2">
+                  <li>{routine.itemid1}</li>
+                  <li>{routine.itemid3}</li>
+                  <li>{routine.itemid5}</li>
+                  <li>{routine.itemid7}</li>
+                </ul>
+              </div>
+              <div className="container2">
+                <ul className="progressbar2">
+                  <li>{routine.itemid2}</li>
+                  <li>{routine.itemid4}</li>
+                  <li>{routine.itemid6}</li>
+                  <li>{""}</li>
+                </ul>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     );
   }
 }
